f650137cc033af0eb7bb6bae26261c0d886b46aa
Rodrigo Mecheri <rodrigobme@hotmail.com>
2024-11-18 21:15:18 +0100

    FEAT: GlobalUsing.cs

---
+++ Cinema.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using Microsoft.EntityFrameworkCore;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.CQRS;
+global using Cinema.API.Data;
+global using Carter;
+global using MediatR;
