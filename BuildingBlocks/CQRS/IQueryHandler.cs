e90188e0fb2f0ecb14065fbc3678521f82e18e8b
Rodrigo Mecheri <rodrigobme@hotmail.com>
2024-11-18 21:11:54 +0100

    FEAT: BuildingBlocks.csproj

---
+++ BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+		where TResponse : notnull
+		where TQuery : IQuery<TResponse>
+{
+}
